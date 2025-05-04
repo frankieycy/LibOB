dbc9fc3587b8350ff6c5e60107bd42fa7077f139
Frankie Yeung <frankieycy@gmail.com>
2025-05-04 10:23:48 -0400

    MatchingEngine interface

--- lib/MatchingEngine.cpp
+++ lib/MatchingEngine.cpp
@@ -6,25 +6,15 @@
 #include "MatchingEngine.hpp"
 
 namespace Exchange {
-MatchingEngineBase::MatchingEngineBase(const MatchingEngineBase& matchingEngine) :
-    mySymbol(matchingEngine.mySymbol),
-    myExchangeId(matchingEngine.myExchangeId),
-    myBidLimitOrderBook(matchingEngine.myBidLimitOrderBook),
-    myAskLimitOrderBook(matchingEngine.myAskLimitOrderBook),
-    myMarketOrderQueue(matchingEngine.myMarketOrderQueue),
-    myTradesLog(matchingEngine.myTradesLog) {}
-
-void MatchingEngineBase::process(const std::shared_ptr<Market::OrderBase>& order) {
+void IMatchingEngine::process(const std::shared_ptr<Market::OrderBase>& order) {
     order->submit(*this);
 }
 
-void MatchingEngineBase::addToLimitOrderBook(const std::shared_ptr<Market::LimitOrder>& order) {}
+std::ostream& IMatchingEngine::orderBookSnapshot(std::ostream& out) const {
+    return out;
+}
 
-void MatchingEngineBase::executeMarketOrder(const std::shared_ptr<Market::MarketOrder>& order) {}
-
-void MatchingEngineBase::init() {}
-
-const std::string MatchingEngineBase::getAsJason() const {
+const std::string IMatchingEngine::getAsJason() const {
     return "";
 }
 }
--- lib/MatchingEngine.hpp
+++ lib/MatchingEngine.hpp
@@ -5,23 +5,38 @@
 #include "Trade.hpp"
 
 namespace Exchange {
-class MatchingEngineBase {
+class IMatchingEngine {
 public:
-    MatchingEngineBase() = default;
-    MatchingEngineBase(const MatchingEngineBase& matchingEngine);
-    virtual std::shared_ptr<MatchingEngineBase> clone() const { return std::make_shared<MatchingEngineBase>(*this); }
+    IMatchingEngine() = default;
+    IMatchingEngine(const IMatchingEngine& matchingEngine) = default;
+    virtual std::shared_ptr<IMatchingEngine> clone() const = 0;
     virtual void process(const std::shared_ptr<Market::OrderBase>& order);
-    virtual void addToLimitOrderBook(const std::shared_ptr<Market::LimitOrder>& order);
-    virtual void executeMarketOrder(const std::shared_ptr<Market::MarketOrder>& order);
-    virtual void init();
+    virtual void addToLimitOrderBook(const std::shared_ptr<Market::LimitOrder>& order) = 0;
+    virtual void executeMarketOrder(const std::shared_ptr<Market::MarketOrder>& order) = 0;
+    virtual void init() {};
+    virtual std::ostream& orderBookSnapshot(std::ostream& out) const;
     virtual const std::string getAsJason() const;
+    friend std::ostream& operator<<(std::ostream& out, const IMatchingEngine& matchingEngine);
 private:
+    using PriceLevel = double;
+    using LimitQueue = std::deque<std::shared_ptr<Market::LimitOrder>>;
+    using DescOrderBook = std::map<PriceLevel, LimitQueue, std::greater<double>>;
+    using AscOrderBook = std::map<PriceLevel, LimitQueue>;
     std::string mySymbol;
     std::string myExchangeId;
-    std::map<double, std::deque<const std::shared_ptr<Market::LimitOrder>>, std::greater<double>> myBidLimitOrderBook;
-    std::map<double, std::deque<const std::shared_ptr<Market::LimitOrder>>> myAskLimitOrderBook;
-    std::deque<Market::MarketOrder> myMarketOrderQueue;
-    std::vector<Market::TradeBase> myTradesLog;
+    DescOrderBook myBidBook;
+    AscOrderBook myAskBook;
+    std::deque<std::shared_ptr<Market::MarketOrder>> myMarketOrders;
+    std::vector<std::shared_ptr<Market::TradeBase>> myTrades;
+};
+
+class MatchingEngineFIFO : public IMatchingEngine {
+public:
+    MatchingEngineFIFO() = default;
+    MatchingEngineFIFO(const MatchingEngineFIFO& matchingEngine) = default;
+    virtual std::shared_ptr<IMatchingEngine> clone() const override { return std::make_shared<MatchingEngineFIFO>(*this); }
+    virtual void addToLimitOrderBook(const std::shared_ptr<Market::LimitOrder>& order) override {}
+    virtual void executeMarketOrder(const std::shared_ptr<Market::MarketOrder>& order) override {}
 };
 }
 
