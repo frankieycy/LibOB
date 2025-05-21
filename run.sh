7bf352a61888b3adc617649b4ecb8794766ad14f
Frankie Yeung <frankieycy@gmail.com>
2025-05-21 02:10:27 -0400

    run.sh: cpp paths

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]; then
 else
 	file=$1
 fi
-g++ -Ilib -std=c++17 -Wall -Wextra -O2 lib/*.cpp run/${file}.cpp -o exe/${file}
+g++ -Ilib -std=c++17 -Wall -Wextra -O2 $(find run lib -name '*.cpp') -o exe/${file}
 if [ $? -eq 0 ]; then
 	./exe/${file} 2>&1 | tee run.log
 fi
