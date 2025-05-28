e443274e1382a012bec55523458ad299c0073658
Frankie Yeung <frankieycy@gmail.com>
2025-05-28 00:28:53 -0400

    Logger: log file setter

--- lib/Utils/Logger.hpp
+++ lib/Utils/Logger.hpp
@@ -14,6 +14,13 @@ public:
     virtual void log(const std::string& message, const LogLevel level = LogLevel::INFO, const OverwriteLastLog overwrite = OverwriteLastLog::NO);
     virtual std::string getTimestamp() const;
     void setSilent(const bool silent) { myIsSilent = silent; }
+    void setLogFile(const std::string& logFileName, const bool showLogTimestamp = true) {
+        if (myLogToFile && myLogFile.is_open())
+            myLogFile.close();
+        myLogFileName = logFileName;
+        myLogFile.open(myLogFileName, std::ios::out | std::ios::app);
+        myShowLogTimestamp = showLogTimestamp;
+    }
     LoggerStream operator<<(const LogLevel& level) {
         return LoggerStream(*this, level);
     }
