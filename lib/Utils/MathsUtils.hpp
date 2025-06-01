4a5a5b820bcd09215c96185c8361b02ae0a1393a
Frankie Yeung <frankieycy@gmail.com>
2025-06-01 15:38:56 -0400

    MathsUtils: add castDoublePriceAsInt

--- lib/Utils/MathsUtils.hpp
+++ lib/Utils/MathsUtils.hpp
@@ -8,6 +8,11 @@ inline double roundPriceToTick(double price, double tick = 0.01) {
     const auto ticks = std::llround(price / tick);
     return ticks * tick;
 }
+
+template <typename T>
+inline T castDoublePriceAsInt(double price, double multiplier = 10000.0) {
+    return static_cast<T>(std::llround(price * multiplier));
+}
 }
 }
 
