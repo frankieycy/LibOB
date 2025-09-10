7ab809b54d3921b974704c464ac331b2b07eca84
Frankie Yeung <frankieycy@gmail.com>
2025-09-10 10:36:24 -0400

    add LobsterDataParser class

---
+++ lib/Parser/LobsterDataParser.cpp
@@ -0,0 +1,15 @@
+#ifndef LOBSTER_DATA_PARSER_CPP
+#define LOBSTER_DATA_PARSER_CPP
+#include "LobsterDataParser.hpp"
+
+namespace Parser {
+std::string LobsterDataParser::OrderBookMessage::getAsCsv() const {
+    return ""; // TODO
+}
+
+std::string LobsterDataParser::OrderBookSnapshot::getAsCsv() const {
+    return ""; // TODO
+}
+}
+
+#endif
---
+++ lib/Parser/LobsterDataParser.hpp
@@ -0,0 +1,40 @@
+#ifndef LOBSTER_DATA_PARSER_HPP
+#define LOBSTER_DATA_PARSER_HPP
+#include "Utils/Utils.hpp"
+
+namespace Parser {
+class LobsterDataParser {
+    enum class MessageType {
+        ORDER_ADD,
+        ORDER_CANCEL,
+        ORDER_DELETE,
+        ORDER_EXECUTE_VISIBLE,
+        ORDER_EXECUTE_HIDDEN,
+        TRADING_HALT
+    };
+
+    struct OrderBookMessage {
+        uint64_t timestamp;
+        MessageType messageType;
+        uint64_t orderId;
+        uint32_t quantity;
+        uint32_t price;
+        bool isBuy;
+        std::string getAsCsv() const;
+    };
+
+    struct OrderBookSnapshot {
+        std::vector<uint32_t> askPrice;
+        std::vector<uint32_t> askSize;
+        std::vector<uint32_t> bidPrice;
+        std::vector<uint32_t> bidSize;
+
+        OrderBookSnapshot(size_t levels) :
+            askPrice(levels), askSize(levels),
+            bidPrice(levels), bidSize(levels) {}
+        std::string getAsCsv() const;
+    };
+};
+}
+
+#endif
