9e11d62035dae4d99239bf7c857c58468ccae89a
Frankie Yeung <frankieycy@gmail.com>
2025-12-14 02:29:08 -0500

    ExchangeSimulator: imrpove interface

--- Lib/Simulator/ExchangeSimulator.cpp
+++ Lib/Simulator/ExchangeSimulator.cpp
@@ -9,6 +9,13 @@
 namespace Simulator {
 using namespace Utils;
 
+void IExchangeSimulator::setConfig(const ExchangeSimulatorConfig& config) {
+    myConfig = config;
+    myMatchingEngine->setDebugMode(myConfig.debugMode);
+    myMatchingEngineMonitor->setOrderBookNumLevels(myConfig.monitoredLevels);
+    myOrderEventManager->setMinimumPriceTick(myConfig.grid.minPriceTick);
+}
+
 void IExchangeSimulator::init() {
     if (!myMatchingEngine)
         Error::LIB_THROW("[IExchangeSimulator] Matching engine is null during simulator initialization.");
@@ -21,16 +28,11 @@ void IExchangeSimulator::init() {
 
 void IExchangeSimulator::reset() {
     // empties out the matching engine and re-initializes the simulator
+    if (myState == ExchangeSimulatorState::RUNNING)
+        Error::LIB_THROW("[IExchangeSimulator] Cannot reset simulator while it is running.");
     if (myMatchingEngine)
         myMatchingEngine->reset();
     init();
-}
-
-void IExchangeSimulator::setConfig(const ExchangeSimulatorConfig& config) {
-    myConfig = config;
-    myMatchingEngine->setDebugMode(myConfig.debugMode);
-    myMatchingEngineMonitor->setOrderBookNumLevels(myConfig.monitoredLevels);
-    myOrderEventManager->setMinimumPriceTick(myConfig.grid.minPriceTick);
 }
 }
 
--- Lib/Simulator/ExchangeSimulator.hpp
+++ Lib/Simulator/ExchangeSimulator.hpp
@@ -1,6 +1,7 @@
 #ifndef EXCHANGE_SIMULATOR_HPP
 #define EXCHANGE_SIMULATOR_HPP
 #include "Utils/Utils.hpp"
+#include "Market/OrderEvent.hpp"
 #include "Market/OrderEventManager.hpp"
 #include "Exchange/MatchingEngine.hpp"
 #include "Analytics/MatchingEngineMonitor.hpp"
@@ -8,6 +9,7 @@
 
 namespace Simulator {
 using namespace Utils;
+using Timestamp = uint64_t;
 
 enum class ExchangeSimulatorState { UNINITIALIZED, READY, RUNNING, FINISHED };
 enum class ExchangeSimulatorType { ZERO_INTELLIGENCE, MINIMAL_INTELLIGENCE, NULL_EXCHANGE_SIMULATOR_TYPE };
@@ -25,6 +27,12 @@ struct ExchangeSimulatorConfig {
     OrderBookGridDefinition grid;
 };
 
+class IEventScheduler {
+public:
+    virtual ~IEventScheduler() = default;
+    virtual std::optional<Market::OrderEventBase> nextEvent(const Timestamp currentTimestamp) = 0;
+};
+
 class IExchangeSimulator {
 public:
     IExchangeSimulator() = default;
@@ -33,14 +41,6 @@ public:
         init();
     }
     virtual ~IExchangeSimulator() = default;
-    virtual void init();
-    virtual void reset();
-    virtual void initOrderBookBuilding() = 0;
-    virtual void step() = 0;
-    virtual void runSimulation(const uint64_t numEvents) = 0;
-    virtual void runUntil(const uint64_t timestamp) = 0;
-    virtual ExchangeSimulatorState getState() const noexcept = 0;
-    virtual uint64_t getCurrentTimestamp() const noexcept = 0;
 
     std::shared_ptr<const Exchange::IMatchingEngine> getMatchingEngine() const { return myMatchingEngine; }
     std::shared_ptr<const Market::OrderEventManagerBase> getOrderEventManager() const { return myOrderEventManager; }
@@ -48,8 +48,21 @@ public:
     const ExchangeSimulatorConfig& getConfig() const { return myConfig; }
     void setConfig(const ExchangeSimulatorConfig& config);
 
+    virtual void init();
+    virtual void reset();
+    virtual void initOrderBookBuilding() = 0;
+    virtual void advanceByEvent() = 0; // advance by one event (tick in event time)
+    virtual void advanceToTimestamp(const Timestamp timestamp) = 0; // advance to timestamp (tick in wall-clock time)
+    virtual void simulateByEvent(const uint64_t numEvents) = 0;
+    virtual void simulateUntilTimestamp(const Timestamp timestamp) = 0;
+    virtual ExchangeSimulatorState getState() const = 0;
+    virtual Timestamp getCurrentTimestamp() const = 0;
+    virtual uint64_t getRandomSeed() const = 0;
+    virtual void setRandomSeed(const uint64_t seed) = 0;
+
 private:
     ExchangeSimulatorConfig myConfig;
+    ExchangeSimulatorState myState = ExchangeSimulatorState::UNINITIALIZED;
     std::shared_ptr<Exchange::IMatchingEngine> myMatchingEngine;
     std::shared_ptr<Market::OrderEventManagerBase> myOrderEventManager;
     std::shared_ptr<Analytics::MatchingEngineMonitor> myMatchingEngineMonitor;
