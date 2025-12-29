656fea2da80af3b62160b77b7425d00bd8e8b041
Frankie Yeung <frankieycy@gmail.com>
2025-12-29 01:50:25 -0500

    ZeroIntelligenceUtils: interfaces for rate, side, size, price

---
+++ Lib/Simulator/ZeroIntelligenceUtils.hpp
@@ -0,0 +1,64 @@
+#ifndef ZERO_INTELLIGENCE_UTILS_HPP
+#define ZERO_INTELLIGENCE_UTILS_HPP
+#include "Utils/Utils.hpp"
+#include "Market/OrderUtils.hpp"
+
+/* Interfaces for drawing the sizes/prices in the event generation of the zero-intelligence simulator.
+   One may specify the marginal distributions by having the explicit parameters in the concrete distribution
+   classes, or the conditional distributions dependent on the book shape by having the MatchingEngineMonitor
+   as a class member. */
+namespace Simulator {
+class IOrderEventRateSampler {
+public:
+    virtual ~IOrderEventRateSampler() = default;
+    virtual double sample() const = 0;
+};
+
+class ConstantOrderEventRateSampler : public IOrderEventRateSampler {
+public:
+    ConstantOrderEventRateSampler(const double rate) : myRate(rate) {}
+    virtual ~ConstantOrderEventRateSampler() = default;
+    virtual double sample() const override { return myRate; }
+private:
+    double myRate;
+};
+
+class IOrderSideSampler {
+public:
+    virtual ~IOrderSideSampler() = default;
+    virtual Market::Side sample() const = 0;
+};
+
+class UniformOrderSideSampler : public IOrderSideSampler {
+public:
+    virtual ~UniformOrderSideSampler() = default;
+    virtual Market::Side sample() const override {
+        return Statistics::getRandomUniform01(true) < 0.5 ? Market::Side::BUY : Market::Side::SELL;
+    }
+};
+
+class IOrderSizeSampler {
+public:
+    virtual ~IOrderSizeSampler() = default;
+    virtual uint32_t sample(Market::Side side) const = 0;
+};
+
+class IOrderPricePlacementSampler {
+public:
+    virtual ~IOrderPricePlacementSampler() = default;
+    virtual double sample(Market::Side side) const = 0;
+};
+
+struct OrderCancelSpec {
+    uint32_t quantity;
+    double price;
+};
+
+class IOrderCancellationSampler {
+public:
+    virtual ~IOrderCancellationSampler() = default;
+    virtual std::optional<OrderCancelSpec> sample(Market::Side side) const = 0; // optional to allow for empty book
+};
+}
+
+#endif
