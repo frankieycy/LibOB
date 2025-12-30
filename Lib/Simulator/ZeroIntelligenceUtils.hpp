700ea9db0aede6871ddfe5365998f8752045e1cf
Frankie Yeung <frankieycy@gmail.com>
2025-12-30 13:08:36 -0500

    ZeroIntelligenceUtils: add engine-aware interfaces

--- Lib/Simulator/ZeroIntelligenceUtils.hpp
+++ Lib/Simulator/ZeroIntelligenceUtils.hpp
@@ -2,16 +2,38 @@
 #define ZERO_INTELLIGENCE_UTILS_HPP
 #include "Utils/Utils.hpp"
 #include "Market/OrderUtils.hpp"
+#include "Analytics/MatchingEngineMonitor.hpp"
 
 /* Interfaces for drawing the sizes/prices in the event generation of the zero-intelligence simulator.
    One may specify the marginal distributions by having the explicit parameters in the concrete distribution
    classes, or the conditional distributions dependent on the book shape by having the MatchingEngineMonitor
-   as a class member. */
+   as a class member - see the EngineAware classes. */
 namespace Simulator {
+class IEngineAwareSampler {
+public:
+    IEngineAwareSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) : myMonitor(monitor) {}
+    virtual ~IEngineAwareSampler() = default;
+    const Analytics::MatchingEngineMonitor::OrderBookTopLevelsSnapshot& getLastOrderBookTopLevelsSnapshot() const {
+        return myMonitor->getLastOrderBookTopLevelsSnapshot();
+    }
+    const Statistics::TimeSeriesCollector<Analytics::MatchingEngineMonitor::OrderBookStatisticsByTimestamp>& getOrderBookStatistics() const {
+        return myMonitor->getOrderBookStatistics();
+    }
+protected:
+    std::shared_ptr<Analytics::MatchingEngineMonitor> myMonitor;
+};
+
 class IOrderEventRateSampler {
 public:
     virtual ~IOrderEventRateSampler() = default;
     virtual double sample() const = 0;
+};
+
+class IEngineAwareOrderEventRateSampler : public IOrderEventRateSampler, public IEngineAwareSampler {
+public:
+    IEngineAwareOrderEventRateSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) :
+        IOrderEventRateSampler(), IEngineAwareSampler(monitor) {}
+    virtual ~IEngineAwareOrderEventRateSampler() = default;
 };
 
 class ConstantOrderEventRateSampler : public IOrderEventRateSampler {
@@ -29,6 +51,13 @@ public:
     virtual Market::Side sample() const = 0;
 };
 
+class IEngineAwareOrderSideSampler : public IOrderSideSampler, public IEngineAwareSampler {
+public:
+    IEngineAwareOrderSideSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) :
+        IOrderSideSampler(), IEngineAwareSampler(monitor) {}
+    virtual ~IEngineAwareOrderSideSampler() = default;
+};
+
 class UniformOrderSideSampler : public IOrderSideSampler {
 public:
     virtual ~UniformOrderSideSampler() = default;
@@ -41,6 +70,13 @@ class IOrderSizeSampler {
 public:
     virtual ~IOrderSizeSampler() = default;
     virtual uint32_t sample(Market::Side side) const = 0;
+};
+
+class IEngineAwareOrderSizeSampler : public IOrderSizeSampler, public IEngineAwareSampler {
+public:
+    IEngineAwareOrderSizeSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) :
+        IOrderSizeSampler(), IEngineAwareSampler(monitor) {}
+    virtual ~IEngineAwareOrderSizeSampler() = default;
 };
 
 class NullOrderSizeSampler : public IOrderSizeSampler {
@@ -71,6 +107,13 @@ public:
     virtual double sample(Market::Side side) const = 0;
 };
 
+class IEngineAwareOrderPricePlacementSampler : public IOrderPricePlacementSampler, public IEngineAwareSampler {
+public:
+    IEngineAwareOrderPricePlacementSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) :
+        IOrderPricePlacementSampler(), IEngineAwareSampler(monitor) {}
+    virtual ~IEngineAwareOrderPricePlacementSampler() = default;
+};
+
 class NullOrderPricePlacementSampler : public IOrderPricePlacementSampler {
 public:
     virtual ~NullOrderPricePlacementSampler() = default;
@@ -89,6 +132,13 @@ class IOrderCancellationSampler {
 public:
     virtual ~IOrderCancellationSampler() = default;
     virtual std::optional<OrderCancelSpec> sample(Market::Side side) const = 0; // optional to allow for empty book
+};
+
+class IEngineAwareOrderCancellationSampler : public IOrderCancellationSampler, public IEngineAwareSampler {
+public:
+    IEngineAwareOrderCancellationSampler(const std::shared_ptr<Analytics::MatchingEngineMonitor>& monitor) :
+        IOrderCancellationSampler(), IEngineAwareSampler(monitor) {}
+    virtual ~IEngineAwareOrderCancellationSampler() = default;
 };
 
 class NullOrderCancellationSampler : public IOrderCancellationSampler {
