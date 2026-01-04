620a628c4acd6fbbb46da20d502d5240bd6d7e51
Frankie Yeung <frankieycy@gmail.com>
2026-01-03 22:26:28 -0500

    OrderBookDerivedAnalytics: derived stats interface

--- Lib/Analytics/OrderBookDerivedAnalytics.hpp
+++ Lib/Analytics/OrderBookDerivedAnalytics.hpp
@@ -12,11 +12,22 @@ struct OrderBookTraces {
     Statistics::TimeSeriesCollector<Exchange::OrderProcessingReport> orderProcessingReportsCollector;
 };
 
+struct IOrderBookDerivedStats {
+    virtual ~IOrderBookDerivedStats() = default;
+    virtual void init() = 0;
+    virtual void reset() = 0;
+    virtual void compute() = 0;
+};
+
 /* Multi-horizon price returns aggregator to study the scaling law of returns:
     Var(return_dt) ~ dt^H with H being the Hurst exponent.
     Each horizon dt yields a sumReturns, sumSqReturns etc. */
-struct PriceReturnScalingStats {
+struct PriceReturnScalingStats : public IOrderBookDerivedStats {
     enum class PriceType { LAST_TRADE, MID, MICRO, NONE };
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+
     std::vector<uint64_t> horizons;
     std::vector<double> sumReturns;
     std::vector<double> sumSqReturns;
@@ -26,40 +37,68 @@ struct PriceReturnScalingStats {
 };
 
 /* Event time (number of events) between each price tick (mid or best or whatever). */
-struct EventTimeStats {
+struct EventTimeStats : public IOrderBookDerivedStats {
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+
     Statistics::Histogram eventsBetweenPriceMoves;
 };
 
 /* Autocorrelation of trade signs: +1 for buy and -1 for sell. */
-struct OrderFlowMemoryStats {
+struct OrderFlowMemoryStats : public IOrderBookDerivedStats {
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+
     Statistics::Autocorrelation<int8_t> tradeSignsACF;
 };
 
 /* Average depth profile in price ticks measured from opposite best price. */
-struct OrderDepthProfileStats {
+struct OrderDepthProfileStats : public IOrderBookDerivedStats {
     enum class DepthNormalization { BY_TOTAL_DEPTH, BY_BEST_LEVEL, NONE };
     enum class PriceSpaceDefinition { DIFF_TO_MID, DIFF_TO_OWN_BEST, DIFF_TO_OPPOSITE_BEST, NONE };
-    std::vector<double> avgBid;
-    std::vector<double> avgAsk;
-    std::vector<double> stdBid;
-    std::vector<double> stdAsk;
-    std::vector<size_t> nonZeroCountBid;
-    std::vector<size_t> nonZeroCountAsk;
+    size_t getNumSnapshots() const { return numSnapshots; }
+    void accumulate(const OrderBookTopLevelsSnapshot& /* snapshot */) {} // TODO
+    virtual void init() override {}
+    virtual void reset() override {}
+    virtual void compute() override {}
+
+    std::vector<double> avgBid, avgAsk;
+    std::vector<double> stdBid, stdAsk;
+    std::vector<size_t> nonZeroCountBid, nonZeroCountAsk;
     size_t maxTicks = 0; // size of the depth profile in price ticks
-    size_t numSnapshots; // number of snapshots used to compute the profile
     bool countMissingLevels = true;
     DepthNormalization normalization = DepthNormalization::NONE;
     PriceSpaceDefinition priceSpace = PriceSpaceDefinition::NONE;
+
+private:
+    size_t numSnapshots = 0; // number of snapshots used to compute the profile
+    std::vector<double> sumBid, sumAsk;
+    std::vector<double> sumSqBid, sumSqAsk;
 };
 
 /* Spread statistics in space (histogram) and time (autocorrelation). */
-struct SpreadStats {
+struct SpreadStats : public IOrderBookDerivedStats {
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+
     Statistics::Histogram spreadHistogram;
     Statistics::Autocorrelation<double> spreadACF;
 };
 
-struct OrderLifetimeStats;
-struct PriceImpactStats;
+struct OrderLifetimeStats : public IOrderBookDerivedStats {
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+};
+
+struct PriceImpactStats : public IOrderBookDerivedStats {
+    virtual void init() override {} // TODO
+    virtual void reset() override {}
+    virtual void compute() override {}
+};
 }
 
 #endif
