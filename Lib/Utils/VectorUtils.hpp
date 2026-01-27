2573f58992d011cec95ac6460500f6abb901e0cc
Frankie Yeung <frankieycy@gmail.com>
2026-01-26 23:32:31 -0500

    VectorUtils: namespace wrap

--- Lib/Utils/VectorUtils.hpp
+++ Lib/Utils/VectorUtils.hpp
@@ -2,9 +2,11 @@
 #define VECTOR_UTILS_HPP
 #include <vector>
 
+namespace Utils {
 namespace Vector {
 std::vector<double> getVectorRange(const double a, const double b, const double x);
 std::vector<double> getVectorRange(const double a, const double b, const int n);
+}
 }
 
 #endif
